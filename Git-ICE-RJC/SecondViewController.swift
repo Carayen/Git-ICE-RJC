6cad250ed0ad6f511fa2f46728eaff28606ddc1e
Carley,Ryan J <S516892@mail.nwmissouri.edu>
2016-11-14 13:25:10 -0600

    Initial Commit

---
+++ Git-ICE-RJC/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Git-ICE-RJC
+//
+//  Created by Carley,Ryan J on 11/14/16.
+//  Copyright © 2016 Carley,Ryan J. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
