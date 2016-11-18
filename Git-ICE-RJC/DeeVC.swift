61f92a252547d6ad145b25ad134cb4c92b895ed5
Lalit <tanakaryuji91@gmail.com>
2016-11-18 13:18:21 -0600

    Add a fourth ViewController
    Will add a fourth view controller with a button and a text field. This view is connected to the fourth button on the tab.

---
+++ Git-ICE-RJC/DeeVC.swift
@@ -0,0 +1,39 @@
+//
+//  DeeVC.swift
+//  Git-ICE-RJC
+//
+//  Created by Kandriga,Naga Krishna Lalith on 11/18/16.
+//  Copyright © 2016 Carley,Ryan J. All rights reserved.
+//
+
+import UIKit
+
+class DeeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet var TextInputTF: UITextField!
+
+    @IBAction func clickBTN(sender: AnyObject) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
